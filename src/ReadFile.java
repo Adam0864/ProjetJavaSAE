c8f1e30c327c816c16a2c1c1789d64ab37c1a3d6
Adam <ahachicha4@outlook.fr>
2025-04-30 10:28:12 +0200

    debut du projet

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,5 +1,6 @@
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.ArrayList;
 import java.util.Scanner; // Import the Scanner class to read text files
 
 public class ReadFile {
@@ -7,10 +8,22 @@ public class ReadFile {
         try {
             File myObj = new File("src/Données/distances.txt");
             Scanner myReader = new Scanner(myObj);
+            ArrayList<String> villes = new ArrayList<>();
+            ArrayList<ArrayList<Integer>> distances = new ArrayList<>();
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                System.out.println(data);
+                int compteur = 0;
+                for (String mot : data.split(" ")) {
+                    // Vérifier si la ligne est un nombre entier
+                    if (mot.matches("-?\\d+")) {  // Vérifie si c'est un entier
+                        int number = Integer.parseInt(mot);
 
+
+                }
+                    else{
+                        villes.add(mot);
+                    }
+                break;
             }
             myReader.close();
         } catch (FileNotFoundException e) {

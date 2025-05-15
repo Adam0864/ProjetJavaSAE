fc9045e50015ae9f88600aa465936cf1d3c51db5
Adam <ahachicha4@outlook.fr>
2025-05-15 11:57:57 +0200

    RPOJET SAE SCAN FICHIER

--- src/Algo.java
+++ src/Algo.java
@@ -1,6 +1,17 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Algo {
-    private scannerFichier scenario;
+    private Map<String, String> chScenario;
+    private Map<String, String> chMembreAppli;
+    private Map<String, ArrayList<Integer>> chDistanceVille;
+
+    public Algo(String parScenarioChoisi) throws FileNotFoundException {
+        chScenario = new scannerFichier().lectureScenario(parScenarioChoisi);
+        chMembreAppli = new scannerFichier().membreappli();
+        chDistanceVille = new scannerFichier().distanceville();
+    }
 
 }

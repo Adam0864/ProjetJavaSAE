0fc53890620b59a29ac544f88fb85acba758835b
Adam <ahachicha4@outlook.fr>
2025-05-15 11:38:33 +0200

    RPOJET SAE SCAN FICHIER

---
+++ src/Client.java
@@ -0,0 +1,12 @@
+import java.io.FileNotFoundException;
+import java.util.Map;
+
+public class Client {
+    public static void main(String[] args) throws FileNotFoundException {
+        Map<String, Integer> villeindices = new scannerFichier().indicesVilles(new scannerFichier().distanceville());
+        System.out.println(villeindices.get("Bordeaux"));
+        Map<String, String> scenariochoisi = new scannerFichier().lectureScenario("s0");
+        System.out.println(scenariochoisi);
+
+    }
+}
--- src/scannerFichier.java
+++ src/scannerFichier.java
@@ -5,16 +5,20 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class scanner {
-    public static void main(String[] args) throws FileNotFoundException {
+public class scannerFichier {
+    public Map<String, ArrayList<Integer>> distanceville(){
         /*Lecture des distances du fichier distances.txt
-        * on obtient un dictionnaire */
-
+         * on obtient un dictionnaire */
         File distance = new File("src/Données/distances.txt");
-        Scanner scan = new Scanner(distance);
+        Scanner scan = null;
+        try {
+            scan = new Scanner(distance);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
         Map<String, ArrayList<Integer>> distanceville = new LinkedHashMap<>();
-        ArrayList<String> listVilles = new ArrayList<>();
+
 
         while (scan.hasNextLine()) {
             String line = scan.nextLine();
@@ -22,7 +26,6 @@ public class scanner {
 
             if (split.length > 0) {
                 String ville = split[0];
-                listVilles.add(ville);
                 ArrayList<Integer> listdistance = new ArrayList<>();
 
                 for (int i = 1; i < split.length; i++) {
@@ -32,17 +35,23 @@ public class scanner {
                 distanceville.put(ville, listdistance);
             }
         }
+        return distanceville;
+    }
+
+    public Map<String, Integer> indicesVilles(Map<String, ArrayList<Integer>> distanceville) {
         /*Indices des villes pour obtenir les distances de chaque villes par rapport à une autre */
-        Map<String,Integer> indicesVilles = new LinkedHashMap<>();
+        Map<String, Integer> indices = new LinkedHashMap<>();
         int index = 0;
 
-        for (String ville : listVilles) {
-            indicesVilles.put(ville, index);
-            index++;
+        for (String ville : distanceville.keySet()) {
+            indices.put(ville, index++);
         }
 
-        /*Lecture des distances du fichier distances.txt */
+        return indices;
+    }
 
+    public Map<String, String> membreappli() throws FileNotFoundException {
+        /*Lecture des distances du fichier distances.txt permettant d'obtenir un dictionnaire memberdic tel que (Membre,Ville)*/
         File memberliste = new File("src/Données/membres_APPLI.txt");
         Scanner scan2 = new Scanner(memberliste);
         Map<String, String> memberdic = new LinkedHashMap<>();
@@ -51,13 +60,17 @@ public class scanner {
             String[] split = line.split(" ");
             memberdic.put(split[0], split[1]);
         }
-        System.out.println(memberdic);
-        System.out.println(memberdic.keySet());
-        System.out.println(memberdic.values());
+        return memberdic;
+    }
 
-        /*Lecture des scénarios du fichier scénario.txt */
-        File scenario = new File("src/Données/scenario_0.txt");
-        Scanner scan3 = new Scanner(scenario);
+    public Map<String, String> lectureScenario(String scenario) throws FileNotFoundException {
+        /*Lecture des scénarios du fichier scénario.txt permettant d'obtenir un dictionnaire transactions tel que (Acheteur,Vendeur) */
+        Map<String,String> scenarios = new LinkedHashMap<>();
+        for (int i=0;i<9;i++) {
+            scenarios.put("s"+i,"scenario_"+i);
+        }
+        File scenarioFile = new File("src/Données/"+scenarios.get(scenario)+".txt");
+        Scanner scan3 = new Scanner(scenarioFile);
 
         Map<String, String> transactions = new LinkedHashMap<>();
 
@@ -66,6 +79,8 @@ public class scanner {
             String[] split = line.split(" -> ");
             transactions.put(split[0], split[1]);
         }
-        System.out.println(transactions);
+        return transactions;
     }
+
+
 }

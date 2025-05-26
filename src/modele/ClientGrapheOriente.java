4be08373f50437a39ef82d3c17e3a57de8f81ee4
ahach <ahachicha4@outlook.fr>
2025-05-26 23:35:46 +0200

    projet sae

--- src/modele/ClientGrapheOriente.java
+++ src/modele/ClientGrapheOriente.java
@@ -2,7 +2,7 @@ package modele;
 
 public class ClientGrapheOriente {
     public static void main(String[] args) throws Exception {
-        GrapheOriente g = new GrapheOriente(new Scenario("s7"));
+        GrapheOriente g = new GrapheOriente(new Scenario("s8"));
 
         System.out.println(g.triTopologique());
     }
--- src/modele/GrapheOriente.java
+++ src/modele/GrapheOriente.java
@@ -6,14 +6,14 @@ public class    GrapheOriente {
     private TreeMap<String, LinkedHashSet<String>> chVoisinsSortant;
     private ArrayList<String> chSommets;
     private Map<String, Ville> chDistance;
-    private Map<String,Integer> chDegreEntrant;
+    private Map<String, Integer> chDegreEntrant;
 
     public GrapheOriente(Scenario parScenario) throws Exception {
         // 1) Initialisation des champs
-        chVoisinsSortant    = new TreeMap<>();
-        chSommets           = new ArrayList<>();
-        chDistance          = new LinkedHashMap<>();
-        chDegreEntrant      = new LinkedHashMap<>();
+        chVoisinsSortant = new TreeMap<>();
+        chSommets = new ArrayList<>();
+        chDistance = new LinkedHashMap<>();
+        chDegreEntrant = new LinkedHashMap<>();
 
         // 2) A partir de Velizy
         chDistance.put("VelizyV", new Ville("Velizy"));
@@ -62,7 +62,6 @@ public class    GrapheOriente {
             }
 
 
-
         }
         // garantir que chaque sommet a bien un ensemble (même vide) dans la liste d'adjacence
         for (String sommet : chSommets) {
@@ -71,17 +70,17 @@ public class    GrapheOriente {
     }
 
 
-    public Map<String,Integer> getDegreEntrant(){
+    public Map<String, Integer> getDegreEntrant() {
         return chDegreEntrant;
     }
 
 
-    public LinkedHashSet<String> getChVoisinsSortant(String parVille){
+    public LinkedHashSet<String> getChVoisinsSortant(String parVille) {
         return chVoisinsSortant.get(parVille);
     }
 
     public String triTopologique() {
-        Map<String,Integer> degEnt = getDegreEntrant();
+        Map<String, Integer> degEnt = getDegreEntrant();
         int distancetotal = 0;
 
         // 1) File des sources (degrés 0)
@@ -99,85 +98,27 @@ public class    GrapheOriente {
             ordre.add(s);
             for (String v : this.getChVoisinsSortant(s)) {
                 degEnt.put(v, degEnt.get(v) - 1);
-                distancetotal += chDistance.get(s).getChDistanceVille(chDistance.get(v));
                 if (degEnt.get(v) == 0) {
                     sources.addLast(v);
                 }
             }
         }
 
-        // 3) Détection de cycle
-        if (ordre.size() < chSommets.size()) {
-            throw new IllegalStateException("Cycle détecté : tri impossible");
+        for (int i = 0; i < ordre.size() - 1; i++) {
+            String de = ordre.get(i);
+            String vers = ordre.get(i+1);
+            Ville vDe  = chDistance.get(de);
+            Ville vVers= chDistance.get(vers);
+            distancetotal += vDe.getChDistanceVille(vVers);
         }
+
+
+        // 3) Détection de cycle
+        /*if (ordre.size() < chSommets.size()) {
+            throw new IllegalStateException("Cycle détecté : tri impossible");
+        }*/
         return ordre + "Distance totale :" + distancetotal;
     }
 
-
-    /*public String toString() {
-        String resultat="";
-        for (int indiceSommet: chVoisinsSortant.keySet()) {
-            resultat+="sommet "+indiceSommet+" degre sortant="+this.Degre(indiceSommet)+" voisins : ";
-            for (int voisin: chVoisinsSortant.get(indiceSommet)) {
-                resultat+=voisin+",";
-            }
-            resultat+="\n";
-        }
-        return resultat;
-    }
-
-
-
-    public TreeMap<Integer, Pair<Integer,Integer>> Parcour_largeur() {
-        List<Integer> file=new ArrayList<>();
-        file.add(0);
-        int [] Distance=new int[chVoisinsSortant.size()-1];
-        Distance[0]=0;
-        int [] Predeceur=new int[chVoisinsSortant.size()-1];
-        while if (file != null) {
-
-        }
-    }
-
-    public TreeMap<Integer, Integer> getDegreEntrant() {
-        TreeMap<Integer, Integer> degreEntrant=new TreeMap<>();
-        for (int indiceSommet:chVoisinsSortant.keySet()) {
-            degreEntrant.put(indiceSommet,0);
-        }
-        for (int indiceSommet: chVoisinsSortant.keySet()) {
-            for (int voisin: chVoisinsSortant.get(indiceSommet)) {
-                degreEntrant.put(voisin, degreEntrant.get(voisin)+1);
-            }
-        }
-        return degreEntrant;
-    }
-
-    public TreeSet<Integer> getSources(TreeMap<Integer, Integer> degreEntrant) {
-        TreeSet<Integer> sources=new TreeSet<>();
-        for (int indiceSommet: degreEntrant.keySet()) {
-            if (degreEntrant.get(indiceSommet)==0) {
-                sources.add(indiceSommet);
-            }
-        }
-        return sources;
-    }
-
-    public ArrayList<Integer> triTopologique(){
-        TreeMap<Integer,Set<Integer>> parVoisinsSortant = chVoisinsSortant;
-        TreeMap<Integer,Integer> degresEntrants = this.getDegreEntrant();
-        TreeSet<Integer> sources = this.getSources(degresEntrants);
-        ArrayList<Integer> num = new ArrayList<>();
-        while (!sources.isEmpty()) {
-            int s = sources.pollFirst();
-            for (int v : parVoisinsSortant.get(s)) {
-                degresEntrants.put(v, degresEntrants.get(v)-1);
-                if (degresEntrants.get(v)==0){
-                    sources.add(v);
-                }
-            }
-            num.add(s);
-        }
-        return num;
-    }*/
 
 }
